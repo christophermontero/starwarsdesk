b734614841e17db4572e771f2245e559c27efa76
Christopher Ortiz <cgortizm21@gmail.com>
2023-11-05 22:39:43 -0500

    style(footer): Add the github repo link

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
-  repository: string = '';
+  repository: string = 'https://github.com/christophermontero/starwars-desk';
 }
