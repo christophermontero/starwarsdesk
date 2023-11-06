905a17ae74cd8e31807ac775f1bdf16d6ad9d6a2
Christopher Ortiz <cgortizm21@gmail.com>
2023-11-05 21:30:54 -0500

    feat(services): Add a service to fetch spacecraft's information

--- src/app/services/spacecraft.service.ts
+++ src/app/services/spacecraft.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ISpacecraft } from '../interfaces/spacecraft';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpacecraftService {
+  private _url: string = 'assets/data/spacecrafts.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getSpacecrafts() {
+    return this.http.get<ISpacecraft[]>(this._url);
+  }
 }
