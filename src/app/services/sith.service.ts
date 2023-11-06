aba21188bf4e852d2e28dfacd16c201f001169db
Christopher Ortiz <cgortizm21@gmail.com>
2023-11-05 21:28:35 -0500

    feat(services): Add a service to fetch sith's information

--- src/app/services/sith.service.ts
+++ src/app/services/sith.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICharacter } from '../interfaces/characters';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SithService {
+  private _url: string = 'assets/data/siths.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getSiths() {
+    return this.http.get<ICharacter[]>(this._url);
+  }
 }
